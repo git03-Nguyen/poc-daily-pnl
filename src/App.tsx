1882b4f4eadc24d19ed007fffac85b36125c6025
DinhAnh <anh.nguyen@lfglobaltech.com>
2025-04-16 14:40:55 +0700

    OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Statistics from './pages/Statistics'
 
 function App() {
